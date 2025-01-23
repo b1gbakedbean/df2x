8048ce81be453bc68f7fdfac4fa13dfca6599ef8
b1gbakedbean <153480135+b1gbakedbean@users.noreply.github.com>
2025-01-23 07:00:40 -0600

    Lower the delay used for certain network updates

--- src/game_dll/components/network.cpp
+++ src/game_dll/components/network.cpp
@@ -64,6 +64,21 @@ namespace df2x::components
 		utils::memory::set<uint8_t>(0x0040869A, 1);
 		utils::memory::set<uint8_t>(0x004086BF, 1);
 
+		// Lower the delay used for transmission of certain network updates (client)
+		utils::memory::set<uint8_t>(0x004421F5, 1);
+
+		// Lower the delay used for transmission of certain network updates (server).
+		// The default value for NovaWorld servers is 13 (~4.8pps) while 1 is the lowest possible value
+		// and causes certain network updates to be sent as fast as the games tick rate (~62.5hz) acheiving ~62.5pps.
+		// NOTE: A stock client cannot connect if the server is using a value below 7.
+		utils::memory::set<uint8_t>(0x00441F7F, 1);
+
+		// Lower the delay between checking for network messages and sending any queued messages
+		// during the loading screen (client). The default is 100ms, this patch is a 4ms delay or 25x faster.
+		// NOTE: This is required by the client if the server is using a value below 7 as mentioned above.
+		utils::memory::set<uint8_t>(0x004089DB, 4);
+		utils::memory::set<uint8_t>(0x00408B3E, 4);
+
 		// Allow the use of the alternate startup.htm (without this you cannot connect through NovaHQ)
 		utils::memory::set<uintptr_t>(0x0043E215, 0x0043DDB0);
 		utils::memory::set<uintptr_t>(0x0043E221, 0x0043DB50);
