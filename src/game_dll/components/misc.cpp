2b139f1f2e14737e53abcc216946be38435816f4
b1gbakedbean <153480135+b1gbakedbean@users.noreply.github.com>
2025-02-11 21:58:39 -0600

    Include version in window title; Prevent the game from overriding our title

--- src/game_dll/components/misc.cpp
+++ src/game_dll/components/misc.cpp
@@ -1,16 +1,25 @@
 #include "misc.hpp"
 #include "utils/memory.hpp"
+#include "version.hpp"
 
 namespace df2x::components
 {
 	void misc::load()
 	{
+		// Prevent the game from overriding our title
+		utils::memory::nop(0x00424A01, 5);
+		utils::memory::nop(0x004249ED, 5);
+
 		// Change the window title
-		utils::memory::set_string(0x004C329C, "DF2x, ");
+		utils::memory::set_string(0x00A6E3C0, "DF2x, ");
+#ifndef _DEBUG
+		utils::memory::set_string(0x00A6E4C0, DF2X_FULL_VERSION);
+#else
+		utils::memory::set_string(0x00A6E4C0, fmt::format("{}-{}", DF2X_FULL_VERSION, DF2X_GIT_REVISION).c_str());
+#endif
 	}
 
 	void misc::unload()
 	{
-
 	}
 }
